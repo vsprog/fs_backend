b036383a3e8e743778decb69ebb73629495ea31a
quicker123456789 <chmolekula@gmail.com>
2018-05-12 18:00:20 +0700

    headers added

--- packages/core/core.js
+++ packages/core/core.js
@@ -17,10 +17,23 @@ app.use('/', moviesRoutes);   // bookmarks/movies,  /.*/ - любой путь
 app.use((req, res) => {
   res.json({ status: 'BAD_REQUEST', messages: [error({ code: 'BAD_REQUEST' })] });
 });
-//отключение eslint на следующую строку 
+//отключение eslint на следующую строку
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   res.json({ status: 'FAIL', messages: [error({ code: err.message })] });
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+// router
+router.use((req, res, next) => {
+  setTimeout(next, 300);
 });
 
 module.exports = app;
